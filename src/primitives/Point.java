32c95bb8c26027f567a381a510f4d91abbd15448
nsx nsx <sz184790@gmail.com>
2022-02-24 11:51:53 +0200

    11:51

---
+++ src/primitives/Point.java
@@ -0,0 +1,36 @@
+package primitives;
+
+import java.util.Objects;
+
+public class Point {
+    Double3 xyz;
+
+    public  Point(Double3 double3){
+        xyz=double3;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Point)) return false;
+        Point other = (Point)obj;
+        return this.xyz.equals(other.xyz);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xyz);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "xyz=" + xyz +
+                '}';
+    }
+
+
+}
+
+
